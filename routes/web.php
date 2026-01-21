e46b1d6744a584ae2c4c4102416a0b960c46a908
sherifabdelbast <sherifabdelbast@yahoo.com>
2026-01-21 19:43:26 +0200

    setting up local environemnt and using sail for docker container

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/test', function () {
+   
+return  response()->json(['message' => 'Test route is working!']);
+});
+
+Route::get('/' , function () {
     return view('welcome');
 });
