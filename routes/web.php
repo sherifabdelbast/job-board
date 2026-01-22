3f8982dce7711dee754062e6e7ca07f071fd9ca6
sherifabdelbast <sherifabdelbast@yahoo.com>
2026-01-22 21:37:50 +0200

    using breeze and sanctum for scaffolding authentication and making API endpoints

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
-   
-return  response()->json(['message' => 'Test route is working!']);
+    return response()->json([
+        'message' => 'Welcome to the Job Board API'
+    ]);
 });
 
-Route::get('/' , function () {
-    return view('welcome');
-});
+require __DIR__ . '/auth.php';
